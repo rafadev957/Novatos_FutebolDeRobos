d33ddd6e85f37845010c606973e5b6668b9a9f60
Renato Luciano Cagnin <renato.cagnin90@gmail.com>
2025-05-24 10:53:58 -0300

    Apresentação do alinhamento do robô

--- main.py
+++ main.py
@@ -1,43 +1,22 @@
-import sys, os, time
+import sys, os, time, math
 sys.path.append(os.path.join(os.path.dirname(__file__), 'communication'))
 from communication import Communication
 
 con = Communication("127.0.0.1:20011", "224.0.0.1:10002")
 con.startServer()
 
-t = 0
 while True:
-    if t // 100 % 2 == 0 and t % 100 == 0:
-        #Time amarelo
-        con.sendOne(0, 6.5, 0)
-        con.sendOne(1, 5.0, 0.5)
-        con.sendOne(2, -2, 2)
-        #Time azul
-        con.sendOne(0, -0.5, -0.5, False)
-        con.sendOne(1, 5, -5, False)
-        con.sendOne(2, -0.5, -1.0, False)
-    elif t % 100 == 0:
-        con.sendOne(0, -6.5, 0)
-        con.sendOne(1, -5.0, -0.5)
-        con.sendOne(2, 2, -2)
-        #Time azul
-        con.sendOne(0, 0.5, 0.5, False)
-        con.sendOne(1, -5, 5, False)
-        con.sendOne(2, 0.5, 1.0, False)
-    print(con.frame.ball.x, con.frame.ball.y)
     if len(con.frame.robots_blue) > 0:
-        for i in range(len(con.frame.robots_blue)):
-            print(i, con.frame.robots_blue[i].x, 
-                     con.frame.robots_blue[i].y,
-                     con.frame.robots_blue[i].orientation
-            )
-    print()
-    if len(con.frame.robots_yellow) > 0:
-        for i in range(len(con.frame.robots_yellow)):
-            print(i, con.frame.robots_yellow[i].x, 
-                     con.frame.robots_yellow[i].y,
-                     con.frame.robots_yellow[i].orientation
-            )
-    print(t, "---------------------------------", con.env.step)
-    time.sleep(0.1)
-    t += 1 
+        # vetor no plano (x, y) que liga o robô ao objetivo
+        objx = con.frame.ball.x - con.frame.robots_blue[0].x
+        objy = con.frame.ball.y - con.frame.robots_blue[0].y
+        angulo_obj = math.atan2(objy, objx)
+
+        print("Robô", con.frame.robots_blue[0].orientation, "Obj", angulo_obj)
+        if con.frame.robots_blue[0].orientation >= angulo_obj - 0.09 and con.frame.robots_blue[0].orientation <=  angulo_obj + 0.09:
+            print("ALINHOU!!!!")
+            con.sendOne(0, 0, 0, False)
+            time.sleep(0.1)
+        else:
+            con.sendOne(0, -10.5, 10.5, False)
+    time.sleep(0.01)
