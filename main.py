0a440c31c9a8aa35fb4cab575e57137a682f76a1
rafael <rafaelmberg@gmail.com>
2025-08-18 16:38:58 -0300

    atualizações de visualização e ideias

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from defender import Defender
 from goalkeeper import Goalkeeper
 
 r0 = Attacker()
-
+r2 = Goalkeeper()
 con = Communication("127.0.0.1:20011", "224.0.0.1:10002")
 con.startServer()
 
 r0.setCommunication(con)
+r2.setCommunication(con)
 #adicionar mais robôs: r1.id = 1 ou r2.id = 2
 
 while True:
@@ -18,10 +19,17 @@ while True:
     if len(con.frame.robots_yellow) > 0: #garante que haja algo na lista de robos
 
         r0.setObj(con.frame.ball.x, con.frame.ball.y)
+        r2.setObj(con.frame.ball.x, con.frame.ball.y)
 
         r0.setPose(con.frame.robots_yellow[r0.id].x, 
                    con.frame.robots_yellow[r0.id].y, 
                    con.frame.robots_yellow[r0.id].orientation) 
+        
+        r2.setPose(con.frame.robots_yellow[r2.id].x, 
+                   con.frame.robots_yellow[r2.id].y, 
+                   con.frame.robots_yellow[r2.id].orientation) 
 
-        r0.update()
+        #r0.update()
+        r2.update()
+        print(con.frame.robots_yellow[r2.id].x, con.frame.robots_yellow[r2.id].y, con.frame.robots_yellow[r2.id].orientation)
         time.sleep(0.1)
